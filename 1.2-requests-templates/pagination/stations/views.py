410a216ef53278bde7b3d4e486ebe027424ce74a
Eleonora Harutyunyan <ev.arutyunyan@mail.ru>
2022-03-05 17:48:56 +0300

    Ошибка

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,6 @@
+import csv
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -6,12 +9,19 @@ def index(request):
     return redirect(reverse('bus_stations'))
 
 
+with open('data-398-2018-08-30.csv', 'r', encoding='utf-8') as file:
+    directions = list(csv.DictReader(file, delimiter=','))
+
+
 def bus_stations(request):
+    page_number = request.GET.get("page", 1)
+    paginator = Paginator(directions, 1)
+    page = paginator.get_page(page_number)
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'page': page,
+        'bus_stations': directions,
+
     }
     return render(request, 'stations/index.html', context)
