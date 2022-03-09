81ea5c606c0adbe0b97dd2336eaf9cd503082726
Eleonora Harutyunyan <ev.arutyunyan@mail.ru>
2022-03-09 22:45:43 +0300

    2/3

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,45 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, ArticleTag
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        if len(self.forms) == 0:
+            raise ValidationError('Вы не вввели тег')
+
+        self.count = 0
+        for form in self.forms:
+
+            if self.count > 0 and form.cleaned_data.get('is_main'):  # не могли бы вы объяснить эту конструкцию? Как ведется подсчет в self.count именно по is_main? Данную форму нашла в интернете
+                raise ValidationError('Главный может быть только 1 тег')
+            else:
+                if form.cleaned_data.get('is_main'):
+                    print(f"{form.cleaned_data.get('tags')} - главный раздел")
+                    self.count += 1
+                else:
+                    continue
+        if self.count < 1:
+            raise ValidationError('Должен быть хотя бы один основной тег')
+
+        return super().clean()
+
+
+class ArticleTagInline(admin.TabularInline):
+    model = ArticleTag
+    formset = RelationshipInlineFormset
+    extra = 0
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'published_at']
+    inlines = [ArticleTagInline,]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -7,10 +7,35 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    # scopes
+    # tags
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Раздел')
+    tags = models.ManyToManyField(Article, related_name='tags', through='ArticleTag')
+    # scopes
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleTag(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes', verbose_name='Раздел')
+    is_main = models.BooleanField(verbose_name='Основной')
+
+
